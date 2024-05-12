cbde3b72304d9cecb7e6e422aa25fffe03fce29a
‌‌‌𝓜𝓸𝓱𝓪𝓶𝓶𝓪𝓭 <150838661+m86dosti@users.noreply.github.com>
2024-05-12 19:04:14 +0330

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+
+        function startloop()
+        {
+            var text = document.getElementById("text").value;
+            var number =parseInt(document.getElementById("number").value);
+            var output =document.getElementById("output");
+           if(text && number!=='')
+           {
+             for(var i = 0 ; i<=number ; i++)
+            {output.innerHTML+=i +" : "+text+'<br>';}
+           }
+           else{output.innerHTML="erorr"} 
+        }
+
