3f13d29eeda64e71b9d757c8f3d8e6cca8289188
‌‌‌𝓜𝓸𝓱𝓪𝓶𝓶𝓪𝓭 <150838661+m86dosti@users.noreply.github.com>
2024-09-05 12:38:36 +0330

    Update main.js

--- main.js
+++ main.js
@@ -2,12 +2,13 @@
         function startloop()
         {
             var text = document.getElementById("text").value;
-            var number =parseInt(document.getElementById("number").value);
-            var output =document.getElementById("output");
+            var number = parseInt(document.getElementById("number").value);
+            var output = document.getElementById("output");
+                
            if(text && number!=='')
            {
              for(var i = 0 ; i<=number ; i++)
-            {output.innerHTML+=i +" : "+text+'<br>';}
+             {output.innerHTML+=i +" : "+text+'<br>';}
            }
            else{output.innerHTML="erorr"} 
         }
