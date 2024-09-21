c40b38d652fb76ec44f141ea0333f25b5ea4307b
𝓜𝓸𝓱𝓪𝓶𝓶𝓪𝓭 <150838661+m86dosti@users.noreply.github.com>
2024-09-22 00:47:38 +0330

    Update main.js

--- main.js
+++ main.js
@@ -4,11 +4,9 @@
            var text = document.getElementById("text").value;
            var number = parseInt(document.getElementById("number").value);
            var output = document.getElementById("output");
-           if(text && number!=='')
-           {
+           if(text && number!==''){
              for(var i = 0 ; i<=number ; i++)
-             {output.innerHTML+=i +" : "+text+'<br>';}
-           }
+             {output.innerHTML+=i +" : "+text+'<br>';}}
            else{output.innerHTML="erorr"} 
         }
 
